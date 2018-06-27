f425f39c738cdb15f2c14f327d48a90eb9d5b573
ayadij <ayadijkwel@yahoo.com>
2018-06-27 09:53:28 -0600

    beginning search bar component

--- reducers/weather/src/containers/search_bar.js
+++ reducers/weather/src/containers/search_bar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {term: ''};
+    }
+
+    onInputChanges(event) {
+        console.log(event.target.value)
+        this.setState()
+    }
+
     render() {
         return (
             <form className='input-group'>
-                <input />
+                <input 
+                    placeholder='Get a five-day forcast in your favorite cities'
+                    className='form-control'
+                    value={this.state.term}
+                    onChange={this.onInputChanges}/>
+                <span className='input-group-btn'>
+                    <button type='submit' className='btn btn-stationary'>Submit</button>
+                </span>    
             </form>
         );
     }
 }
+
+
+//https://www.udemy.com/react-redux/learn/v4/t/lecture/4284590?start=0
+//halfway thru
